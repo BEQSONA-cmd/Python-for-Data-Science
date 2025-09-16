252de295244a6c1f03f0b1953c34f32a6c07857b
Beqa <tvildiani2001@gmail.com>
2025-09-16 13:25:52 +0200

    python-1

--- python-1/ex00/give_bmi.py
+++ python-1/ex00/give_bmi.py
@@ -8,20 +8,12 @@ def give_bmi(height: list[int | float],
 
 
 def apply_limit(bmis: list[int | float], limit: int) -> list[bool]:
-
-    arr = []
-
-    for bmi in bmis:
-        if bmi > limit:
-            arr.append(True)
-        else:
-            arr.append(False)
-    return arr
+    return [bmi > limit for bmi in bmis]
 
 
-if __name__ == "__main__":
-    arr1 = [2, 27, 2, 21, 23]
-    arr2 = [2, 3, 4, 5, 6]
+# if __name__ == "__main__":
+#     arr1 = [2, 27, 2, 21, 23]
+#     arr2 = [2, 3, 4, 5, 6]
 
-    arr3 = np.divide(arr1, arr2)
-    print(list(arr3))
+#     arr3 = np.divide(arr1, arr2)
+#     print(list(arr3))
