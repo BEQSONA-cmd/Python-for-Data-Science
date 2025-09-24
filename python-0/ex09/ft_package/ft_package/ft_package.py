1b304a94e036adb8bb994cf3e3219e6005b1c426
Beqa <tvildiani2001@gmail.com>
2025-09-24 18:33:20 +0200

    python-0

--- python-0/ex09/ft_package/ft_package/ft_package.py
+++
@@ -1,7 +0,0 @@
-def count_in_list(lst, value):
-    """Counts occurrences of value in lst."""
-    count = 0
-    for item in lst:
-        if item == value:
-            count += 1
-    return count
